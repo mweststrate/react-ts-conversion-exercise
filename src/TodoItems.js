5dd9bfe29ef1b2c5d8586d26a11994b81af89ca7
Michel Weststrate <mweststrate@gmail.com>
2021-10-16 13:40:30 +0100

    Change TodoItems to func component

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react'
 
-class TodoItems extends Component {
-  createTasks = item => {
-    return (
-      <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
-        {item.text}
-      </li>
-    )
-  }
-  render() {
-    const todoEntries = this.props.entries
-    const listItems = todoEntries.map(this.createTasks)
-
-    return <ul className="theList">{listItems}</ul>
-  }
+function TodoItems({ entries, deleteItem }) {
+  return (
+    <ul className="theList">
+      {entries.map(item => (
+        <li key={item.key} onClick={() => deleteItem(item.key)}>
+          {item.text}
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default TodoItems
