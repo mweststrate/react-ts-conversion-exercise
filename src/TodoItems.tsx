b6ef3a7d58b4b1e9ed40b7c5e75e11399f811622
Michel Weststrate <mweststrate@gmail.com>
2021-10-16 13:53:39 +0100

    Updated deps, function component

--- src/TodoItems.tsx
+++ src/TodoItems.tsx
@@ -1,23 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Item } from './Item'
 
-class TodoItems extends Component<{
-  entries: Item[]
-  deleteItem: (key: string) => void
-}> {
-  createTasks = (item: Item) => {
-    return (
-      <li key={item.key} onClick={() => this.props.deleteItem(item.key)}>
-        {item.text}
-      </li>
-    )
-  }
-  render() {
-    const todoEntries = this.props.entries
-    const listItems = todoEntries.map(this.createTasks)
-
-    return <ul className="theList">{listItems}</ul>
-  }
+function TodoItems({ entries, deleteItem }: { entries: Item[]; deleteItem: (key: string) => void }) {
+  return (
+    <ul className="theList">
+      {entries.map(item => (
+        <li key={item.key} onClick={() => deleteItem(item.key)}>
+          {item.text}
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default TodoItems
